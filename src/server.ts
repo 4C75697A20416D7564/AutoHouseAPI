427988652147629db0da3c003d709ecd9b526c0d
Luiz Felipe Moralez Amud <amud@MacBook-Pro-de-Luiz.local>
2025-05-24 18:23:30 -0300

    Deus obrigadox2, mais refatoração,doc  swagger)

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 // Importa o framework Express.
-import express from "express";
-import { router } from "./router";
-
+import express from 'express';
+import { router } from './router';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger.json';
 // Cria a aplicação Express.
 const app = express();
 
 // Permite que a aplicação receba dados no formato JSON no corpo da requisição.
 app.use(express.json());
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 // Inicia o servidor na porta 3333 e mostra uma mensagem no console.
-app.listen(3333, () => console.log("Server is Running\nPORT -> 3333"));
+app.listen(3333, () => console.log('Server is Running\nPORT -> 3333'));
